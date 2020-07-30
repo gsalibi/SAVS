5edc76c935a53b2a5297d61ba290c9a093a57fcf
Gustavo <gustavohstorti@gmail.com>
2020-07-30 01:15:11 -0300

    add validation

--- savs_app/static/scripts/displayed_elements.js
+++ savs_app/static/scripts/displayed_elements.js
@@ -54,6 +54,10 @@ function updateRequired(isIdentified) {
         document.getElementById("id_identified_address_number").required = true
         document.getElementById("id_identified_email").required = true
         document.getElementById("id_identified_telephone").required = true
+        document.getElementById("id_identified_institute").required = true
+        document.getElementById("id_identified_ra").required = true
+        document.getElementById("id_identified_course").required = true
+        document.getElementById("id_anonymous_support_requested_complement").required = false
     } else {
         document.getElementById("id_identified_name").required = false
         document.getElementById("id_identified_cpf").required = false
@@ -61,6 +65,10 @@ function updateRequired(isIdentified) {
         document.getElementById("id_identified_address_number").required = false
         document.getElementById("id_identified_email").required = false
         document.getElementById("id_identified_telephone").required = false
+        document.getElementById("id_identified_institute").required = false
+        document.getElementById("id_identified_ra").required = false
+        document.getElementById("id_identified_course").required = false
+        document.getElementById("id_anonymous_support_requested_complement").required = true
     }
 }
 
@@ -69,24 +77,40 @@ function displayUniversityInfos(id_base) {
 
     if (selected === "Aluna(o) de graduação" || selected === "Aluna(o) de pós-graduação") {
         document.getElementById(id_base + "_connection_unicamp_complement").style.display = "none";
+        document.getElementById(id_base + "_connection_unicamp_complement").required = false
         document.getElementById(id_base + "_course").style.display = "block";
+        document.getElementById(id_base + "_course").required = true
         document.getElementById(id_base + "_ra").style.display = "block";
+        document.getElementById(id_base + "_ra").required = true
         document.getElementById(id_base + "_institute").style.display = "block";
+        document.getElementById(id_base + "_institute").required = true
     } else if (selected === "Docente") {
         document.getElementById(id_base + "_connection_unicamp_complement").style.display = "none";
+        document.getElementById(id_base + "_connection_unicamp_complement").required = false
         document.getElementById(id_base + "_course").style.display = "none";
+        document.getElementById(id_base + "_course").required = false
         document.getElementById(id_base + "_ra").style.display = "block";
+        document.getElementById(id_base + "_ra").required = true
         document.getElementById(id_base + "_institute").style.display = "block";
+        document.getElementById(id_base + "_institute").required = true
     } else if (selected === "Outro") {
         document.getElementById(id_base + "_connection_unicamp_complement").style.display = "block";
+        document.getElementById(id_base + "_connection_unicamp_complement").required = true
         document.getElementById(id_base + "_course").style.display = "none";
+        document.getElementById(id_base + "_course").required = false
         document.getElementById(id_base + "_ra").style.display = "none";
+        document.getElementById(id_base + "_ra").required = false
         document.getElementById(id_base + "_institute").style.display = "none";
+        document.getElementById(id_base + "_institute").required = false
     } else {
         document.getElementById(id_base + "_connection_unicamp_complement").style.display = "none";
+        document.getElementById(id_base + "_connection_unicamp_complement").required = false
         document.getElementById(id_base + "_course").style.display = "none";
+        document.getElementById(id_base + "_course").required = false
         document.getElementById(id_base + "_ra").style.display = "none";
+        document.getElementById(id_base + "_ra").required = false
         document.getElementById(id_base + "_institute").style.display = "none";
+        document.getElementById(id_base + "_institute").required = false
     }
 
 }
@@ -105,8 +129,29 @@ function displayOpenField(element, options, fieldID) {
 }
 
 function displayOpenFieldChecked(element, fieldID) {
-    if (element.checked)
+    if (element.checked) {
         document.getElementById(fieldID).style.display = "block";
-    else
+        document.getElementById(fieldID).required = true;
+    } else {
         document.getElementById(fieldID).style.display = "none";
+        document.getElementById(fieldID).required = false;
+    }
+}
+
+function displayDateComplement(selectedDateOption) {
+    if (selectedDateOption === "Eu sei a data exata") {
+        document.getElementById("id_anonymous_episode_date_complement").type = "date";
+    } else if (selectedDateOption === "Eu não sei a data exata, mas sei aproximadamente") {
+        document.getElementById("id_anonymous_episode_date_complement").type = "text";
+        document.getElementById("id_anonymous_episode_date_complement").placeholder = "Se possível, indique se foi há um mês, há mais de 6 meses, há 1 ano etc";
+    }
+}
+
+function displayDatePeriodComplement(selectedDateOption) {
+    if (selectedDateOption === "Eu sei o horário exato") {
+        document.getElementById("id_anonymous_episode_date_period_complement").type = "time";
+    } else if (selectedDateOption === "Eu não sei o horário, mas sei aproximadamente") {
+        document.getElementById("id_anonymous_episode_date_period_complement").type = "text";
+        document.getElementById("id_anonymous_episode_date_period_complement").placeholder = "Se possível, indique se foi de manhã, à tarde, à noite etc";
+    }
 }
